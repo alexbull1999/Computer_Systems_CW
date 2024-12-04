7ee35cb595eca83df06dd71ed8a170c04bca15ff
Alex Bull <alexander.bull24@imperial.ac.uk>
2024-12-04 10:37:50 +0000

    Added a Job struct, allowing each Job to have a unique JobID

--- ProducerConsumer.cpp
+++ ProducerConsumer.cpp
@@ -9,11 +9,18 @@
 
 using namespace std;
 
+// Create a Job struct, to enable us to assign jobIDs to each job
+// ensuring each job is produced only once, and consumed only once
+struct Job {
+	int jobID; // Unique jobID
+	int processingTime; // to be randomly assigned between 1-10
+};
+
 // Create a CircularQueue class using arrays
 
 class CircularQueue{
 	private:
-		int* queue; // pointer to the queue
+		Job* queue; // pointer to the queue, which is an array of Job structs
 		int front; // tracking the index position of the front of the queue
 		int rear; // tracking the index position of the rear of the queue
 		int num_items; // tracking the number of items in the queue
@@ -29,7 +36,7 @@ class CircularQueue{
 		//Constructor: front and rear set to -1 to indicate queue empty
 		CircularQueue(int capacity) : front(-1), rear(-1), num_items(0),
 			capacity(capacity) {
-				queue = new int[capacity];
+				queue = new Job[capacity];
 			}
 
 		//Destructor, as creating CircularQueue queue array on the heap
@@ -39,7 +46,7 @@ class CircularQueue{
 		}
 
 		// method to add a value (job) to the queue
-		void addToQueue(int value) {
+		void addToQueue(const Job& job) {
 			// check if queue is empty, if so update front. Otherwise only update rear
 			if (front == -1) {
 				front = 0;
@@ -47,15 +54,15 @@ class CircularQueue{
 			// % capacity allows the queue to be circular, as when rear reaches
 			// capacity, it will circle back round to 0, provided the array is not full
 			rear = (rear + 1) % capacity;
-			queue[rear] = value; //insert the value (job) into the queue
+			queue[rear] = job; //insert the job into the queue
 			num_items++;
 			totalProduced++;
 		}
 
-		int removeFromQueue() {
+		Job removeFromQueue() {
 			// update front index to take into account item removed, and spaces at
 			// front of queue are available again for the rear if it circles back
-			int job = queue[front];
+			Job job = queue[front];
 			front = (front + 1) % capacity;
 			num_items--;
 			totalConsumed++;
@@ -77,13 +84,13 @@ class CircularQueue{
 
 };
 
-int CircularQueue::totalProduced = 0;
+int CircularQueue::totalProduced = 0; //initializing the static int counters
 int CircularQueue::totalConsumed = 0;
 
 // Declaring the producer function, to be defined later
 void producer(int id, int numJobs, CircularQueue& queue, binary_semaphore& queueMutex,
 		counting_semaphore<>& spacesInQueue, counting_semaphore<>& itemsInQueue, 
-		binary_semaphore& coutMutex);
+		binary_semaphore& coutMutex, int& globalJobID);
 
 //Declaring the consumer function, to be defined later
 void consumer(int id, CircularQueue& queue, binary_semaphore& queueMutex,
@@ -116,6 +123,8 @@ int main(int argc, char* argv[]) {
 	// printing of timeout messages
 	binary_semaphore coutMutex(1);
 
+	int globalJobID = 1; // counter for unique Job IDs 
+
 	srand(time(0)); // seeding random number to be used in producer function
 							
 	// create a vector of thread objects to store and manage the threads
@@ -128,7 +137,7 @@ int main(int argc, char* argv[]) {
 		try {
 			threads.push_back(thread(producer, count + 1, numJobs, ref(circQueue),
 						ref(queueMutex), ref(spacesInQueue), ref(itemsInQueue), 
-						ref(coutMutex)));
+						ref(coutMutex), ref(globalJobID)));
 		}
 		catch (const system_error& error) {
 			cerr << "Error creating producer thread " << count + 1 << ": " <<
@@ -186,11 +195,11 @@ int main(int argc, char* argv[]) {
 
 void producer(int id, int numJobs, CircularQueue& queue, binary_semaphore& queueMutex,
 		counting_semaphore<>& spacesInQueue, counting_semaphore<>& itemsInQueue, 
-		binary_semaphore& coutMutex) {
+		binary_semaphore& coutMutex, int& globalJobID) {
 
 	// loop to carry out the number of jobs required of each producer
 	for (int i = 0; i < numJobs; i++) {
-		int job = rand() % 10 + 1; // ensures job takes a random number between 1-10
+		int processingTime = rand() % 10 + 1; //job processingTime is random 1-10
 		
 		// Try to acquire an empty space in the queue
 		if (!spacesInQueue.try_acquire_for(chrono::seconds(10))) {
@@ -202,8 +211,11 @@ void producer(int id, int numJobs, CircularQueue& queue, binary_semaphore& queue
 
 		// If empty space successfully acquired, lock the queue for mutual exclusion
 		queueMutex.acquire();
+		int jobID = globalJobID++;
+		Job job = {jobID, processingTime};
 		queue.addToQueue(job); 
-		cout << "Producer " << id << " added job with processing time " << job << endl;
+		cout << "Producer " << id << " added job with ID " << jobID <<
+			" and processing time " << processingTime << endl;
 
 		// we can now release the queue semaphore, for others to access
 		queueMutex.release();
@@ -242,15 +254,15 @@ void consumer(int id, CircularQueue& queue, binary_semaphore& queueMutex,
 		// queue for mutual exclusion
 		
 		queueMutex.acquire();
-		int job = queue.removeFromQueue();
-		cout << "Consumer " << id << " is consuming job with time " << job << 
-			" seconds.\n";
+		Job job = queue.removeFromQueue();
+		cout << "Consumer " << id << " is consuming job with ID " << job.jobID << 
+			" with time " << job.processingTime << " seconds.\n";
 		// release the mutex queue semaphore
 		queueMutex.release();
 		// increment the empty slots semaphore, as we have just taken a job
 		spacesInQueue.release();
 		// Consumers are supposed to 'sleep' for the value indicated by the job
-		this_thread::sleep_for(chrono::seconds(job));
+		this_thread::sleep_for(chrono::seconds(job.processingTime));
 	}
 }
 
